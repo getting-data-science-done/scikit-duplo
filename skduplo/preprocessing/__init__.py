ee177a5e41420f8fe145e9e7893df9eb30491e94
John Hawkins <hawkins.john.c@gmail.com>
2024-04-19 08:07:28 +1000

    Adding missing init config

--- skduplo/preprocessing/__init__.py
+++ skduplo/preprocessing/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     "TwoColumnRatioDiff",
-    "ColumnConcatenator"
+    "ColumnConcatenator",
+    "LookupEncoder"
 ]
 
 from .two_column_ratio_diff import TwoColumnRatioDiff
 from .column_concatenator import ColumnConcatenator
+from .lookup_encoder import LookupEncoder
